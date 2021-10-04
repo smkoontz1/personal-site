81da4db751226e3e8962e37735990b7869a5a149
Steven Koontz <smkoontz1@gmail.com>
2021-10-03 19:34:07 -0500

    Main blog cards in place.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'blog-home': "url('/assets/images/bg_trees_1.jpg')"
+      }
+    },
   },
   variants: {
     extend: {},
